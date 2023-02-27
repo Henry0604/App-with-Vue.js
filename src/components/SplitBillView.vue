8181e3b76aff650e59dcce6a3847900875d505f9
Henry0406 <enoc0406@hotmail.com>
2023-02-26 20:08:32 -0600

    first commit

---
+++ src/components/SplitBillView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import PeopleView from './PeopleView.vue';
+import Form from './Form.vue';
+
+</script>
+
+<template>
+    <div class="container">
+        <Form />
+        <PeopleView />
+    </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
